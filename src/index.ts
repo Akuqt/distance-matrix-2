e2f8022f67b71f849fc808fdcdcf70ac96e9724c
Italo Alfaro <bestor911@gmail.com>
2022-03-04 20:08:29 -0500

    feat: google distance matrix service

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./matrix";
+export * from "./common";
---
+++ src/matrix.ts
@@ -0,0 +1,24 @@
+import {
+  baseUrl,
+  requestJson,
+  stringifyQueryParams,
+  DistanceMatrixOptions,
+} from "./common";
+
+export default class DistanceMatrixService {
+  private key: string | undefined;
+
+  constructor(key?: string) {
+    this.key = key;
+  }
+
+  public setKey(key: string) {
+    this.key = key;
+  }
+
+  public getDistanceMatrix(options: DistanceMatrixOptions) {
+    if (!this.key) throw new Error("There's no API key.");
+    const query = stringifyQueryParams(options, this.key);
+    return requestJson(baseUrl + query);
+  }
+}
