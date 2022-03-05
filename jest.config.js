982537c4d47206b0d0f5c9c91c2f8069d008c9a6
Italo Alfaro <bestor911@gmail.com>
2022-03-04 20:07:05 -0500

    chore: jest setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: "./",
+  verbose: true,
+  silent: true,
+  detectOpenHandles: false,
+  coverageDirectory: "./coverage",
+  collectCoverage: true,
+};
